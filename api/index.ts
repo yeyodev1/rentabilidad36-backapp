5e5574a43c0c34e8ed2ea50633afeed3be2bee44
Diego Reyes <diegorele13@gmail.com>
2026-06-23 11:54:18 -0500

    fix: minimal api test

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,11 @@
-import app from "../src/index";
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Server is alive");
+});
+
 export default app;
